c9167b70eff290244318c3b02a2080f02d694982
Enderson Florian Solano <efloriansolano@gmail.com>
2024-02-25 23:26:51 -0400

    Agregando vista de benvenida

---
+++ rinapp/lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Constant 
+{
+  //Primary color
+  static var primaryColor =  const Color(0xff296e48);
+
+  //Onboarding Text
+   static var titleOne = "Crea una cuenta";
+   static var descriptionOne = "Escribe tu email para ingresar en Rinapp";
+   static var titleTwo = "¡Haz tu primera solicitud!";
+   static var descriptionTwo = "Haz una solicitud indicando lo que necesitas";
+   static var titleThree = "¡Valora al profesional!";
+   static var descriptionThree =  "No olvides valorar el trabajo del profesional para ayudar a otros usuarios";
+}
